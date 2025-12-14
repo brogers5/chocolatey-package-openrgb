001960b529eb1f0f7d6a242705e8ee29d5a0af41
brogers5 <6869577+brogers5@users.noreply.github.com>
2025-12-13 22:16:54 -0500

    Update releaseNotes

--- update.ps1
+++ update.ps1
@@ -5,7 +5,8 @@ Import-Module au
 $currentPath = (Split-Path $MyInvocation.MyCommand.Definition)
 . $currentPath\helpers.ps1
 
-$softwareRepo = 'CalcProgrammer1/OpenRGB'
+$gitLabSoftwareRepo = 'CalcProgrammer1/OpenRGB'
+$codebergSoftwareRepo = 'OpenRGB/OpenRGB'
 
 function global:au_GetLatest {
     $streams = [ordered] @{
@@ -25,9 +26,9 @@ function global:au_SearchReplace {
     @{
         "$($Latest.PackageName).nuspec" = @{
             '(<packageSourceUrl>)[^<]*(</packageSourceUrl>)'                             = "`$1https://github.com/brogers5/chocolatey-package-$($Latest.PackageName)/tree/v$($Latest.Version)`$2"
-            '(<licenseUrl>)[^<]*(</licenseUrl>)'                                         = "`$1https://gitlab.com/$($softwareRepo)/-/blob/$($Latest.Tag)/LICENSE`$2"
-            '(<projectSourceUrl>)[^<]*(</projectSourceUrl>)'                             = "`$1https://gitlab.com/$($softwareRepo)/-/tree/$($Latest.Tag)`$2"
-            '(<releaseNotes>)[^<]*(</releaseNotes>)'                                     = "`$1https://gitlab.com/$($softwareRepo)/-/releases/$($Latest.Tag)`$2"
+            '(<licenseUrl>)[^<]*(</licenseUrl>)'                                         = "`$1https://gitlab.com/$($gitLabSoftwareRepo)/-/blob/$($Latest.Tag)/LICENSE`$2"
+            '(<projectSourceUrl>)[^<]*(</projectSourceUrl>)'                             = "`$1https://gitlab.com/$($gitLabSoftwareRepo)/-/tree/$($Latest.Tag)`$2"
+            '(<releaseNotes>)[^<]*(</releaseNotes>)'                                     = "`$1https://codeberg.org/$($codebergSoftwareRepo)/releases/tag/$($Latest.Tag)`$2"
             '(<copyright>)[^<]*(</copyright>)'                                           = "`$1Copyright (C) $(Get-Date -Format yyyy) Adam Honse`$2"
             "(\<dependency .+?`"$($Latest.PackageName).portable`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
         }
